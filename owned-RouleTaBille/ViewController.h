83462414d3e93729e079e20d5bc7a17ddbb2d6a0
huyl <huyl@users.noreply.github.com>
2014-06-24 15:53:02 -0500

    Initial Commit

---
+++ owned-RouleTaBille/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  owned-RouleTaBille
+//
+//  Created by Huy on 6/24/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ owned-RouleTaBilleTests/owned_RouleTaBilleTests.m
@@ -0,0 +1,34 @@
+//
+//  owned_RouleTaBilleTests.m
+//  owned-RouleTaBilleTests
+//
+//  Created by Huy on 6/24/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface owned_RouleTaBilleTests : XCTestCase
+
+@end
+
+@implementation owned_RouleTaBilleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
